0030fdacbdfdbb419211f4e081b42d43cf7c9ffd
Gil Dexter <gbd2117@columbia.edu>
2025-12-17 18:26:29 +0000

    added clipped vs. unclipped

--- final_project/evaluation/__init__.py
+++ final_project/evaluation/__init__.py
@@ -1,3 +1,11 @@
-from .evaluation import evaluate_predictions
+from .evaluation import (
+    evaluate_predictions,
+    get_models_and_val_data,
+    get_pred_summary,
+)
 
-__all__ = ["evaluate_predictions"]
+__all__ = [
+    "evaluate_predictions",
+    "get_models_and_val_data",
+    "get_pred_summary",
+]
--- final_project/evaluation/evaluation.py
+++ final_project/evaluation/evaluation.py
@@ -1,6 +1,96 @@
 import numpy as np
 import pandas as pd
+from typing import Tuple
 from sklearn.metrics import mean_tweedie_deviance
+from sklearn.pipeline import Pipeline
+from final_project.data import read_data, split_data
+from final_project.preprocessing import NUM_FEATURES, CAT_FEATURES, RESPONDER
+from final_project.modeling import EVENT_WEIGHT, load_models
+
+
+def get_models_and_val_data(
+    dataset_name: str,
+) -> Tuple[Pipeline, Pipeline, pd.DataFrame, pd.Series,]:
+    """
+    Load data  + fit models, and return validation set.
+
+    Parameters
+    ----------
+    dataset_name : str
+        Dataset to read.
+
+    Returns
+    -------
+    glm : Pipeline
+        Fitted GLM pipeline.
+    lgbm : Pipeline
+        Fitted LGBM pipeline.
+    X_val : pd.DataFrame
+        Val features.
+    y_val : pd.Series
+        Val responder.
+    """
+    df = read_data(dataset_name)
+
+    df_train, _, df_val = split_data(df, [0.6, 0.0, 0.4])
+
+    X_train = df_train[NUM_FEATURES + CAT_FEATURES].copy()
+    y_train = df_train[RESPONDER]
+
+    sample_weight_train = np.where(
+        X_train["event_code"] != "NONE", EVENT_WEIGHT, 1
+    )
+
+    X_val = df_val[NUM_FEATURES + CAT_FEATURES + ["date"]].copy()
+    y_val = df_val[RESPONDER]
+
+    glm, lgbm = load_models(X_train, y_train, sample_weight_train)
+
+    return glm, lgbm, X_val, y_val
+
+
+def get_pred_summary(
+    glm: Pipeline,
+    lgbm: Pipeline,
+    X_val: pd.DataFrame,
+    y_val: pd.Series,
+) -> pd.DataFrame:
+    """
+    Generate prediction summary DataFrame for validation set.
+
+    Parameters
+    ----------
+    glm : Pipeline
+        Fitted GLM pipeline.
+    lgbm : Pipeline
+        Fitted LGBM pipeline.
+    X_val : pd.DataFrame
+        Validation features.
+    y_val : pd.Series
+        Validation responder.
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+    glm_y_pred = glm.predict(X_val)
+    lgbm_y_pred = lgbm.predict(X_val)
+    date = X_val["date"]
+    X_val = X_val[NUM_FEATURES + CAT_FEATURES].copy()
+
+    df_pred = pd.DataFrame(
+        {
+            "y_true": y_val,
+            "glm_y_pred": glm_y_pred,
+            "lgbm_y_pred": lgbm_y_pred,
+            "baseline_y_pred": X_val["past_50m_span_ewm_vol"],
+            "weight": 1,
+            "date": date,
+            "time_of_day": X_val["time_of_day"],
+        }
+    )
+
+    return df_pred
 
 
 def evaluate_predictions(
